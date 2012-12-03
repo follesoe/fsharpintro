3046a5030b22d6348555bc4e53743232b864b3c1
Jonas Folleso <jonas@follesoe.no>
2012-12-03 09:04:47 +0100

    Added type inference, tuples and if-return example.

--- Presentation.fsx
+++ Presentation.fsx
@@ -32,3 +32,29 @@ let rec fib n =
     | n -> fib(n - 1) + fib(n - 2)
 
 fib 7
+
+// Type inference example
+let mult1 x y = x * y // x:int -> y:int -> int
+let mult2 x y = x * y // x:float -> y:float -> float
+
+let res1 = mult1 4 5
+let res2 = mult2 4.0 5.0
+
+// if-expressions return a value
+let isEven x =
+    let result =
+        if x % 2 = 0 then "Yes it is"
+        else "Not is it not"
+    result
+
+isEven 1
+isEven 2
+
+// Tuples
+let dinner = ("pizza", "taco")
+let item1 = fst dinner
+let item2 = snd dinner
+let food1, food2 = dinner
+
+let tupledAdd(x, y) = x + y
+tupledAdd(3, 7)
