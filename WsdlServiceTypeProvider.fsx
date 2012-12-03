9ae75ef207e014fbcba050211a81e9aa86fda792
Jonas Folleso <jonas@follesoe.no>
2012-12-03 09:27:26 +0100

    Added WsdlService Type Provider Example.

---
+++ WsdlServiceTypeProvider.fsx
@@ -0,0 +1,13 @@
+﻿#r "System.ServiceModel"
+#r "FSharp.Data.TypeProviders"
+
+open System
+open System.ServiceModel
+open Microsoft.FSharp.Linq
+open Microsoft.FSharp.Data.TypeProviders
+
+type tempService = WsdlService<"http://www.w3schools.com/webservices/tempconvert.asmx?wsdl">
+let tempClient = tempService.GetTempConvertSoap();
+let far = tempClient.CelsiusToFahrenheit("25")
+
+printfn "25 celcius is %s farenheit" far
