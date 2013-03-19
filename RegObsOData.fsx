e47c5f5c390f1fce3b795640c874f06f21a23eba
Jonas Folleso <jonas@follesoe.no>
2013-03-19 14:22:11 +0100

    RegObs OData sample.

---
+++ RegObsOData.fsx
@@ -0,0 +1,19 @@
+﻿#r "FSharp.Data.TypeProviders"
+#r "System.Data.Services.Client"
+
+open System
+open Microsoft.FSharp.Linq
+open Microsoft.FSharp.Data.TypeProviders
+
+type RegObs = ODataService<"http://api.nve.no/hydrology/regobs/v0.8.7/Odata.svc">
+let data = RegObs.GetDataContext()
+
+let ava = query {
+    for a in data.AvalancheObs do
+    where (a.Comment.Length > 0)
+    sortByDescending a.DtAvalancheTime
+    take 10
+    select a
+}
+
+ava |> Seq.iter(fun a -> printfn "(%A): %s" a.DtAvalancheTime a.Comment)
